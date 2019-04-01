d503072243c1fc3b8ed49a3164b0cae2cbfea49a
c19981127 <1365902565@qq.com>
2019-04-01 17:48:49 +0800

    libpcap

--- libcap/get_device.c
+++ libcap/get_device.c
@@ -1,5 +1,9 @@
 #include <pcap.h>
+#include <time.h>
+//get ip adress  nad umask
 void get_ip_mask(const char *device,bpf_u_int32 * netp,bpf_u_int32 *maskp,char *errbuf);
+//catch packages
+void get_package(pcap_t * device, char * errbuf);
 int main()
 {
   char errbuf[PCAP_ERRBUF_SIZE],*device;
@@ -19,8 +23,15 @@ int main()
   printf("the ip is %u \n",net);  //here it does not show as "198.168.1.1" but "1123141"
   printf("the mask is %u \n",mask);
 
-  pcap_t * test = pcap_open_live(device , 65535, 1, 10000, errbuf);
-  printf("the pcap_t  fd is %d \n",test);
+  pcap_t * test = pcap_open_live(device , 65535, 1, 0, errbuf); //here we open the device
+  if(!test)
+  {
+    printf("error pcap_open_live() \n");
+    printf("%s \n",errbuf);
+    exit(1);
+  }
+  get_package(test,errbuf);
+  pcap_close(test);
   return 0;
 }
 void get_ip_mask(const char *device ,bpf_u_int32 *netp,bpf_u_int32 *maskp,char * errbuf)
@@ -29,4 +40,18 @@ void get_ip_mask(const char *device ,bpf_u_int32 *netp,bpf_u_int32 *maskp,char *
     printf("pcap_lookupnet successful \n");
   else
     printf("error, the pcap_lookupnet failed \n");
+}
+void get_package(pcap_t * device , char * errbuf)
+{
+  struct pcap_pkthdr packet;
+  const u_char * data = pcap_next(device, &packet);
+  if(!data)
+  {
+    printf("error pacp_next() \n");
+    printf("%s \n",errbuf);
+    exit(1);
+  }
+  printf("packet length : %d \n",packet.len);
+  printf("number of bytes : %d \n",packet.caplen);
+  printf("Recieved time : %s \n",ctime((const time_t *)&packet.ts.tv_sec));
 }
